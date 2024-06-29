1a69ed6ad755b8b986f7d0cc88f6736594c69d92
Aakashraz <aakashraz@outlook.com>
2024-06-29 23:32:36 +0545

    npm package axios

--- http-example.js
+++
@@ -1,8 +0,0 @@
-const { get } = require('https.mjs');
-
-get('https://www.google.com', res => {
-    res.on('data', chunk => console.log(`Data chunk: ${chunk}\n`));
-    res.on('end', () => console.log('No More Data'));
-});
-
-// req.end();
